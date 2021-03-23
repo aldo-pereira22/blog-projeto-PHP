f3997f30dc3815d690734304ce8ec8ca8e83bd39
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-03-23 16:08:28 -0300

    Config inicial
    Configuração inicial do projeto

--- index.php
+++ index.php
@@ -9,7 +9,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BLOG EM PHP</title>
-    <link rel="stylesheet" href="<? $BASE_URL ?>"/css/style.css">
+
+    <!-- Estilo do projeto -->
+    <link rel="stylesheet" href="<?=$BASE_URL?>css/style.css">
+    <!-- Fontes -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <h1>
