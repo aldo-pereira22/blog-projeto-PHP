1ff7d9af7375dc8f73e132701213ab207cf86036
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-03-28 10:32:27 -0300

    Responsividade
    Deixando o blog responsividade

--- post.php
+++ post.php
@@ -1,32 +1,16 @@
-<!-- <?php
-include_once("templates/header.php");
+<?php
+    include_once("templates/header.php");
 
 
-if(isset($_GET['id'])){
-    $postId = $_GET['id'];
-    $currentPost;
-    foreach($posts as $post){
-        if($post['id'] == $postId){
-            $currentPost = $post;
+    if(isset($_GET['id'])){
+        $postId = $_GET['id'];
+        $currentPost;
+        foreach($posts as $post){
+            if($post['id'] == $postId){
+                $currentPost = $post;
+            }
         }
     }
-}
-
-// include_once("templates/header.php");
-
-// if(isset($_GET['id'])){
-
-//   $postId = $_GET['id'];
-//   $currentPost;
-
-//   foreach($posts as $post){
-//     if($post['id'] == $postId){
-//       $currentPost = $post;
-//     }
-//   }
-
-// }
-
 
 ?>
 
@@ -39,70 +23,72 @@ if(isset($_GET['id'])){
                 <p id="post-description"> <?= $currentPost['description'] ?> </p>
                 <div class="img-container">
                     <img src="img/<?=$post['img']?>" alt="<?=$currentPost['img'] ?>">
-
-
-                        <p class="post-content">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Nostrum minus sequi nobis ullam voluptatibus vitae dolores
-                        aspernatur autem eaque enim laborum ducimus illum, distinctio
-                        debitis possimus? Id quod deserunt voluptatibus!
-                                            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Nostrum minus sequi nobis ullam voluptatibus vitae dolores
-                        aspernatur autem eaque enim laborum ducimus illum, distinctio
-                        debitis possimus? Id quod deserunt voluptatibus!
-
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Nostrum minus sequi nobis ullam voluptatibus vitae dolores
-                        aspernatur autem eaque enim laborum ducimus illum, distinctio
-                        debitis possimus? Id quod deserunt voluptatibus!
-
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Nostrum minus sequi nobis ullam voluptatibus vitae dolores
-                        aspernatur autem eaque enim laborum ducimus illum, distinctio
-                        debitis possimus? Id quod deserunt voluptatibus!
-                                            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Nostrum minus sequi nobis ullam voluptatibus vitae dolores
-                        aspernatur autem eaque enim laborum ducimus illum, distinctio
-                        debitis possimus? Id quod deserunt voluptatibus!
-
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Nostrum minus sequi nobis ullam voluptatibus vitae dolores
-                        aspernatur autem eaque enim laborum ducimus illum, distinctio
-                        debitis possimus? Id quod deserunt voluptatibus!
-
-                    </p>
-
                 </div>
 
-    
 
+            <p class="post-content">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Nostrum minus sequi nobis ullam voluptatibus vitae dolores
+                aspernatur autem eaque enim laborum ducimus illum, distinctio
+                debitis possimus? Id quod deserunt voluptatibus!
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Nostrum minus sequi nobis ullam voluptatibus vitae dolores
+                aspernatur autem eaque enim laborum ducimus illum, distinctio
+                debitis possimus? Id quod deserunt voluptatibus!
+
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Nostrum minus sequi nobis ullam voluptatibus vitae dolores
+                aspernatur autem eaque enim laborum ducimus illum, distinctio
+                debitis possimus? Id quod deserunt voluptatibus!
+            </p>
+            <P class="#post-content">
+
+
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Nostrum minus sequi nobis ullam voluptatibus vitae dolores
+                aspernatur autem eaque enim laborum ducimus illum, distinctio
+                debitis possimus? Id quod deserunt voluptatibus!
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Nostrum minus sequi nobis ullam voluptatibus vitae dolores
+                aspernatur autem eaque enim laborum ducimus illum, distinctio
+                debitis possimus? Id quod deserunt voluptatibus!
+                
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Nostrum minus sequi nobis ullam voluptatibus vitae dolores
+                aspernatur autem eaque enim laborum ducimus illum, distinctio
+                debitis possimus? Id quod deserunt voluptatibus!
+                
+            </P>
+                        
         </div>
 
-    <aside id="nav-container">
+        
 
-    <h3 id="tags-title"> Tags </h3>
-     <ul id="tag-list">
-     <?php foreach($currentPost['tags'] as $tag): ?>    
-            <li> 
-                <a href="#"> <?= $tag?> </a>
+            <aside id="nav-container">
 
-            </li>    
-        <?php endforeach; ?>
-    </ul>
+                <h3 id="tags-title"> Tags </h3>
+                <ul id="tag-list">
+                <?php foreach($currentPost['tags'] as $tag): ?>    
+                        <li> 
+                            <a href="#"> <?= $tag?> </a>
 
-    <h3 id="categories-title"> Categorias</h3>
-    <ul id="categories-list">
-        <?php foreach($categories as $category): ?>    
-            <li> 
-                <a href="#"> <?= $category?> </a>
+                        </li>    
+                    <?php endforeach; ?>
+                </ul>
 
-            </li>    
-        <?php endforeach; ?>
-    </ul>
-    </aside>
+                <h3 id="categories-title"> Categorias</h3>
+                <ul id="categories-list">
+                    <?php foreach($categories as $category): ?>    
+                        <li> 
+                            <a href="#"> <?= $category?> </a>
 
+                        </li>    
+                    <?php endforeach; ?>
+                </ul>
 
+            </aside>
     </main>
+
 
 
 
